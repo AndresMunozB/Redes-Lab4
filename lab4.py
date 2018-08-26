3532ba55f4044c6b31b2aefe9058975fa12c1966
Andrés Muñoz <andres.munoz.b@usach.cl>
2018-08-26 14:55:51 -0300

    Comentarios listos

--- lab4.py
+++ lab4.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
+"""==============================================================================
 Created on Sat May  5 18:06:02 2018
 Integrantes:
     Diego Mellis - 18.663.454-3
     Andrés Muñoz - 19.646.487-5
-"""
+=============================================================================="""
 
 from scipy.signal import butter, lfilter
 import matplotlib.pyplot as plt
@@ -20,48 +20,52 @@ from scipy import integrate
 from math import pi
 warnings.filterwarnings('ignore')
 
-#==============================================================================
-# Función: En base a los datos que entrega handel.wav se obtiene			   
-# los datos de la señal, la cantidad de datos que esta tiene, y el tiempo que  
-# que dura el audio.														   
-# Parámetros de entrada: Matriz con los datos de la amplitud del audio.
-# Parámetros de salida: Vector con la señal a trabajar, el largo de la señal y 
-# un vector con los tiempos de la señal, su frecuencia de muestro y su tiempo.
-#==============================================================================
+
+"""==============================================================================
+Función: En base a los datos que entrega el un archivo con el nombre "nameFile" 
+		 se obtiene los datos de la señal, la cantidad de datos que esta tiene, 
+ 		 y el tiempo que dura el audio.
+Entrada: nameFile -> Nombre del archivo 
+Salida:  y_signal -> Valores de la amplitud de la señal (Eje Y) 
+		 rate_signal -> frecuencia de la señal.
+		 time_signal -> Valores del tiempo de la señal (Eje X)
+=============================================================================="""
 def getData(nameFile):
 	rate_signal,y_signal=read(nameFile)
 	len_signal = len(y_signal)
 	time_signal = len_signal/rate_signal
 	return y_signal, rate_signal, time_signal
 
-"""
+"""==============================================================================
 Función: Dada una señal, se obtiene la cantidad de bits para poder representar sus datos.
 Entrada: y_signal -> Señal de la cual se obtiene la cantidad maxima de bits.
 Salida:  len_bin -> Cantidad de bits necesesarios para representar todos los datos.
-"""
+=============================================================================="""
 def getBits(y_signal):
 	maximum = max(y_signal)
 	maxbin = format(maximum,"b")
 	len_bin =  len(maxbin)+1
 	return len_bin #PARA AÑADIR UN BIT DE SIGNO NEGATIVO
-"""
+
+
+"""==============================================================================
 Función: Función que rellena de ceros y modifica el formato de un número negativo binario.
 Entrada: value -> valor que se desea modificar
 		 bits  -> Cantidad de bits que debe tener el nuevo número
 Salida:  binvalue -> valor resultante al remplazar el "-" por el "1" y rellenar con  
 					 ceros hasta obtener un largo igual a "bits".
-"""
+=============================================================================="""
 def binarize(value, bits):
 	binvalue = format(value,"b").zfill(bits)
 	if(binvalue[0]=="-"):
 		binvalue = "1"+binvalue[1:]
 	return binvalue
 
-"""
+"""==============================================================================
 Función: Función que se encarga de transformar la señal a una señal digital.
 Entrada: y_signal -> Señal que se transformará a una señal digital.
 Salida : signalBin -> Señal digitalizada, es decir, tiene solo valores 0 ó 1.
-"""
+=============================================================================="""
 def getArrayBin(y_signal):
 	signalBin = []
 	maxbits = getBits(y_signal)
@@ -71,12 +75,12 @@ def getArrayBin(y_signal):
 			signalBin.append(int(bit))
 	return signalBin
 
-"""
+"""==============================================================================
 Función: Función que grafica y guarda una señal digital.
 Entrada: signalBin -> Señal digital que se desea graficar.
 		 title     -> Título del gráfico.
 		 figura    -> Nombre del archivo que se guardará.
-"""
+=============================================================================="""
 def digitalGraph(signalBin, title, figura):
 	
 	visualBin = []
@@ -94,13 +98,13 @@ def digitalGraph(signalBin, title, figura):
 	savefig(figura)
 	plt.show()
 
-"""
+"""==============================================================================
 Función: Función que grafica y guarda una una señal modulada con o sin ruido.
 Entrada: time2 	   -> Eje x del gráfico.
 		 modulated -> Señal modulada que se desea graficar.
 		 title     -> Título del gráfico.
 		 figura    -> Nombre del archivo que se guardará.
-"""
+=============================================================================="""
 def modulatedGraph(time2,modulated,title,figura):
 	#Aquí se comienza a graficar la señal modulada
 	plt.ylim(-5,5)
@@ -112,26 +116,28 @@ def modulatedGraph(time2,modulated,title,figura):
 	plt.plot(time2[:10000],modulated[:10000])
 	savefig(figura)
 	plt.show()
-"""
+
+
+"""==============================================================================
 Función: Función que agrega ruido a una señal.
 Entrada: signal -> Señal a la cual se le agregará ruido.
 		 snr    -> Relación entre la señal y el ruido.
 Salida : signal_with_noise -> Señal con ruido agregado.
-"""
+=============================================================================="""
 def addNoise(signal, snr):
     noise = random.normal(0.0, 1.0/snr, len(signal))
     signal_with_noise = signal + noise
     return signal_with_noise, noise
 
 
-"""
+"""==============================================================================
 Función: Función que modula una señal digital con el método OOK.
 Entrada: signalBin -> Señal que se modulará.
 Salida : modulated -> Señal modulada. (Eje Y)
 		 time      -> Intervalo de tiempo en un periodo de bit.
 		 f         -> Frecuencia de modulación.
 		 time2     -> Intervalo de valores de tiempo para la nueva señal modulada.
-"""
+=============================================================================="""
 def OOKModulation(signalBin):
 
 	
@@ -151,14 +157,13 @@ def OOKModulation(signalBin):
 	time2 = np.arange(bp/100,bp*count + bp/100, bp/100)
 	return modulated, time, f,time2
 
-"""
+"""==============================================================================
 Función: Función que demodula una señal digital modulada.
 Entrada: signalModulated -> Señal que se modulará.
 		 time -> Intervalo de tiempo en un periodo de bit.
 		 f         -> Frecuencia de modulación.
 Salida : demodulated -> Señal demodulada. 
-		 
-"""
+=============================================================================="""
 def OOKdemodulation(signalModulated,time, f):
 
 	demodulated = []
@@ -183,9 +188,9 @@ def OOKdemodulation(signalModulated,time, f):
 
 
 
-"""
+"""==============================================================================
 Función: Que imprime las opciones del menú.
-"""
+=============================================================================="""
 def printMenu():
 	print("		Menu\n")
 	print("1) Mostrar Señal Digital Original")
